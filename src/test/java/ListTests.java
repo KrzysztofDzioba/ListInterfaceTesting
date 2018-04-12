bdceead80cc8dccfaca8fd8366d0b2921cea6bc4
KrzysztofDzioba <krzysztof.dzioba.93@gmail.com>
2018-04-12 18:01:52 +0200

    Huge refactor part 1

---
+++ src/test/java/ListTests.java
@@ -0,0 +1,150 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ListTests {
+
+    //add(E obj)
+    public static void shouldAddOneObjectToList(List<Integer> list) {
+        list.add(1);
+        assert list.size() == 1;
+    }
+
+
+    //add(int index, E obj)
+    public static void shouldAddObjectOnSpecificPosition(List<Integer> list) {
+        list.add(1);
+        list.add(1);
+        list.add(1, 2);
+        assert list.get(1) == 2;
+    }
+;
+    //addAll(Collection<? extends E> c)
+    public static void addArrayListToExistingCollection(List<Integer> list) {
+        //given
+        List<Integer> secondList = new ArrayList<>();
+        list.add(0);
+        list.add(1);
+        secondList.add(2);
+        secondList.add(3);
+        //when
+        list.addAll(secondList);
+        //then
+        assert list.get(2) == 2;
+    }
+
+
+    // boolean addAll(int index, Collection<? extends E> c);
+    public static void addCollectionAtSpecificIndex(List<Integer> list) {
+        //given
+        list.add(0);
+        list.add(1);
+        List<Integer> secondList = new ArrayList<>();
+        secondList.add(2);
+        secondList.add(3);
+
+        //when
+        list.addAll(1, secondList);
+
+        //then
+        assert list.get(1) == 2;
+    }
+
+    //clear();
+    public static void afterClearingCollectionIsEmpty(List<Integer> list) {
+        list.add(1);
+        list.clear();
+        assert list.size() == 0;
+    }
+
+    //boolean contains(Object o);
+    public static void checkWhetherListContainsSpecificObject(List<Integer> list) {
+        //given
+        list.add(5);
+        boolean contains;
+        //when
+        contains = list.contains(5);
+        //then
+        assert contains;
+    }
+
+    //boolean containsAll(Collection<?> c);
+    public static void containsAllElementsOfPassedCollection(List<Integer> list) {
+        //given
+        List<Integer> newList = simpleArrayListWithBasicValues();
+        list.addAll(newList);
+        boolean containsAll;
+        //when
+        containsAll = list.containsAll(newList);
+        //then
+        assert containsAll;
+    }
+
+//    //boolean equals(Object o);
+//    public static void listIsEqualWithAnotherIfTheyHaveSameObjectsInside(List<Integer> list) {
+//        //given
+//        List<Integer> arrayList = simpleArrayListWithBasicValues();
+//        List<Integer> arrayList2 = simpleArrayListWithBasicValues();
+//        Boolean equals;
+//        //when
+//        equals = arrayList.equals(arrayList2);
+//        //then
+//        assert equals : "Test not passed in arraylist (equals)";
+//    }
+//
+//    //E get(int index);
+//    public static void givesElementOfSpecifiedPosition(List<Integer> list) {
+//        //given
+//        List<Integer> arrayList = new ArrayList<>();
+//        arrayList.add(1);
+//        Integer value;
+//        //when
+//        value = arrayList.get(0);
+//        //then
+//        assert value == 1 : "Test not passed in arraylist";
+//    }
+//
+//    //int hashCode();
+//
+//    //int indexOf(Object o);
+//    public static void returnIndexOfFirstOccurrenceOfElement(List<Integer> list) {
+//        //given
+//        List<Integer> arrayList = simpleArrayListWithBasicValues();
+//        Integer value;
+//        //when
+//        value = arrayList.indexOf(3);
+//        //then
+//        assert value == 2 : "Test not passed in arraylist";
+//    }
+//
+//    //boolean isEmpty();
+//    public static void returnsTrueIfListIsEmpty(List<Integer> list) {
+//        //given
+//        List<Integer> arrayList = new ArrayList<>();
+//        Boolean empty;
+//        //when
+//        empty = arrayList.isEmpty();
+//        //then
+//        assert empty;
+//    }
+//
+//    //boolean isEmpty();
+//    public static void returnsFalseIfListIsNotEmpty(List<Integer> list) {
+//        //given
+//        List<Integer> arrayList = simpleArrayListWithBasicValues();
+//        Boolean empty;
+//        //when
+//        empty = arrayList.isEmpty();
+//        //then
+//        assert !empty;
+//    }
+
+
+
+
+
+    private static ArrayList<Integer> simpleArrayListWithBasicValues() {
+        return new ArrayList<>(Arrays.asList(1,2,3));
+    }
+}
+
--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,40 +1,78 @@
-import sun.awt.image.ImageWatched;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class Tests {
+    private static List<Integer> arrayList;
+    private static List<Integer> linkedList;
+
     public static void main(String[] args) {
         runArrayListTests();
-        runLinkedListTests();
     }
 
     private static void runArrayListTests() {
-        ArrayListTests.shouldAddOneObjectToList();
-        ArrayListTests.shouldAddObjectOnSpecificPosition();
-        ArrayListTests.addOtherCollectionToExistingCollection();
-        ArrayListTests.addCollectionAtSpecificIndex();
-        ArrayListTests.afterClearingCollectionIsEmpty();
-        ArrayListTests.checkWhetherListContainsSpecificObject();
-        ArrayListTests.containsAllElementsOfPassedCollection();
-        ArrayListTests.listIsEqualWithAnotherIfTheyHaveSameObjectsInside();
-        ArrayListTests.givesElementOfSpecifiedPosition();
-        ArrayListTests.returnIndexOfFirstOccurrenceOfElement();
-        ArrayListTests.returnsTrueIfListIsEmpty();
-        ArrayListTests.returnsFalseIfListIsNotEmpty();
+        Tests.createEmptyLists();
 
+        ListTests.shouldAddOneObjectToList(arrayList);
+        ListTests.shouldAddOneObjectToList(linkedList);
+        Tests.createEmptyLists();
+
+        ListTests.shouldAddObjectOnSpecificPosition(arrayList);
+        ListTests.shouldAddObjectOnSpecificPosition(linkedList);
+        Tests.createEmptyLists();
+
+        ListTests.addArrayListToExistingCollection(arrayList);
+        ListTests.addArrayListToExistingCollection(linkedList);
+        Tests.createEmptyLists();
+
+        ListTests.addCollectionAtSpecificIndex(arrayList);
+        ListTests.addCollectionAtSpecificIndex(linkedList);
+        Tests.createEmptyLists();
+
+        ListTests.afterClearingCollectionIsEmpty(arrayList);
+        ListTests.afterClearingCollectionIsEmpty(linkedList);
+        Tests.createEmptyLists();
+
+        ListTests.checkWhetherListContainsSpecificObject(arrayList);
+        ListTests.checkWhetherListContainsSpecificObject(linkedList);
+        Tests.createEmptyLists();
+
+        ListTests.containsAllElementsOfPassedCollection(arrayList);
+        ListTests.containsAllElementsOfPassedCollection(linkedList);
+        Tests.createEmptyLists();
+
+//        Stream.of(arrayList, linkedList).forEach(l -> {
+//            ListTests.shouldAddOneObjectToList(l);
+//            Tests.createEmptyLists();
+//            ListTests.shouldAddObjectOnSpecificPosition(l);
+//            Tests.createEmptyLists();
+//            ListTests.addArrayListToExistingCollection(l);
+//            Tests.createEmptyLists();
+//            ListTests.addCollectionAtSpecificIndex(l);
+//            Tests.createEmptyLists();
+//            ListTests.afterClearingCollectionIsEmpty(l);
+//            Tests.createEmptyLists();
+//            ListTests.checkWhetherListContainsSpecificObject(l);
+//            Tests.createEmptyLists();
+//            ListTests.containsAllElementsOfPassedCollection(l);
+//            Tests.createEmptyLists();
+//            ListTests.listIsEqualWithAnotherIfTheyHaveSameObjectsInside(l);
+//            Tests.createEmptyLists();
+//            ListTests.givesElementOfSpecifiedPosition(l);
+//            Tests.createEmptyLists();
+//            ListTests.returnIndexOfFirstOccurrenceOfElement(l);
+//            Tests.createEmptyLists();
+//            ListTests.returnsTrueIfListIsEmpty(l);
+//            Tests.createEmptyLists();
+//            ListTests.returnsFalseIfListIsNotEmpty(l);
+//            Tests.createEmptyLists();
+//
+//                });
     }
 
-    private static void runLinkedListTests() {
-        LinkedListTests.shouldAddOneObjectToList();
-        LinkedListTests.shouldAddObjectOnSpecificPosition();
-        LinkedListTests.addOtherCollectionToExistingCollection();
-        LinkedListTests.addCollectionAtSpecificIndex();
-        LinkedListTests.afterClearingCollectionIsEmpty();
-        LinkedListTests.checkWhetherListContainsSpecificObject();
-        LinkedListTests.containsAllElementsOfPassedCollection();
-        LinkedListTests.listIsEqualWithAnotherIfTheyHaveSameObjectsInside();
-        LinkedListTests.givesElementOfSpecifiedPosition();
-        LinkedListTests.returnIndexOfFirstOccurrenceOfElement();
-        LinkedListTests.returnsTrueIfListIsEmpty();
-        LinkedListTests.returnsFalseIfListIsNotEmpty();
-
+    private static void createEmptyLists() {
+        Tests.arrayList = new ArrayList<>();
+        Tests.linkedList = new LinkedList<>();
     }
 }
