727d593728c9be239c374006e79c554508efebb1
KrzysztofDzioba <krzysztof.dzioba.93@gmail.com>
2018-04-13 17:43:39 +0200

    Unhash commented tests, small tests refactor

--- src/test/java/ListTests.java
+++ src/test/java/ListTests.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ListTests {
@@ -6,7 +8,7 @@ public class ListTests {
     //add(E obj)
     public static void shouldAddOneObjectToList(List<Integer> list) {
         list.add(1);
-        assert list.size() == 2;
+        assert list.size() == 1;
     }
 
 
@@ -38,7 +40,7 @@ public class ListTests {
         //given
         list.add(0);
         list.add(1);
-        List<Integer> secondList = new ArrayList<>();
+        List<Integer> secondList = emptyList(list);
         secondList.add(2);
         secondList.add(3);
 
@@ -48,102 +50,136 @@ public class ListTests {
         //then
         assert list.get(1) == 2;
     }
-//
-//    //clear();
-//    public static void afterClearingCollectionIsEmpty(List<Integer> list) {
-//        list.add(1);
-//        list.clear();
-//        assert list.size() == 0;
-//    }
-//
-//    //boolean contains(Object o);
-//    public static void checkWhetherListContainsSpecificObject(List<Integer> list) {
+
+    //clear();
+    public static void afterClearingCollectionIsEmpty(List<Integer> list) {
+        list.add(1);
+        list.clear();
+        assert list.size() == 0;
+    }
+
+    //boolean contains(Object o);
+    public static void checkWhetherListContainsSpecificObject(List<Integer> list) {
+        //given
+        list.add(5);
+        boolean contains;
+        //when
+        contains = list.contains(5);
+        //then
+        assert contains;
+    }
+
+    //boolean containsAll(Collection<?> c);
+    public static void containsAllElementsOfPassedCollection(List<Integer> list) {
+        //given
+        List<Integer> newList = simpleListWithBasicValues(list);
+        list.addAll(newList);
+        boolean containsAll;
+        //when
+        containsAll = list.containsAll(newList);
+        //then
+        assert containsAll;
+    }
+
+    //boolean equals(Object o);
+    public static void listIsEqualWithAnotherIfTheyHaveSameObjectsInside(List<Integer> list) {
+        //given
+        List<Integer> list1 = simpleListWithBasicValues(list);
+        List<Integer> list2 = simpleListWithBasicValues(list);
+        boolean listsAreEqual = false;
+        //when
+        if(list1.size() == list2.size() && list1.get(0).equals(list2.get(0)) && list1.get(1).equals(list2.get(1)) &&
+                                                                                list1.get(2).equals(list2.get(2)))
+            listsAreEqual = true;
+        //then
+        assert listsAreEqual;
+    }
+
+    //E get(int index);
+    public static void givesElementOfSpecifiedPosition(List<Integer> list) {
+        //given
+        List<Integer> arrayList = emptyList(list);
+        arrayList.add(1);
+        Integer value;
+        //when
+        value = arrayList.get(0);
+        //then
+        assert value == 1;
+    }
+
+    //int hashCode();
+
+    //int indexOf(Object o);
+    public static void returnIndexOfFirstOccurrenceOfElement(List<Integer> list) {
+        //given
+        list = simpleListWithBasicValues(list);
+        Integer value;
+        //when
+        value = list.indexOf(3);
+        //then
+        assert value == 2;
+    }
+
+    //boolean isEmpty();
+    public static void returnsTrueIfListIsEmpty(List<Integer> list) {
+        boolean empty = false;
+        try {
+            list.get(0);
+        } catch (IndexOutOfBoundsException e) {
+            empty = true;
+        }
+
+        assert empty;
+    }
+
+    //boolean isEmpty();
+    public static void returnsFalseIfListIsNotEmpty(List<Integer> list) {
+        list = simpleListWithBasicValues(list);
+        boolean empty = false;
+        try {
+            list.get(0);
+        } catch (IndexOutOfBoundsException e) {
+            empty = true;
+        }
+
+        assert !empty;
+    }
+
+    //Iterator<E> iterator();
+    public static void methodReturnsIteratorObject(List<Integer> list) {
+        assert list.iterator() != null;
+    }
+
+    //int lastIndexOf(Object o);
+//    public static void testName(List<Integer> list) {
 //        //given
-//        list.add(5);
-//        boolean contains;
-//        //when
-//        contains = list.contains(5);
-//        //then
-//        assert contains;
-//    }
+//        List<Integer> linkedList = simpleListWithBasicValues(list);
 //
-//    //boolean containsAll(Collection<?> c);
-//    public static void containsAllElementsOfPassedCollection(List<Integer> list) {
-//        //given
-//        List<Integer> newList = simpleArrayListWithBasicValues();
-//        list.addAll(newList);
-//        boolean containsAll;
 //        //when
-//        containsAll = list.containsAll(newList);
+//
 //        //then
-//        assert containsAll;
+//        assert false;
 //    }
 
-//    //boolean equals(Object o);
-//    public static void listIsEqualWithAnotherIfTheyHaveSameObjectsInside(List<Integer> list) {
-//        //given
-//        List<Integer> arrayList = simpleArrayListWithBasicValues();
-//        List<Integer> arrayList2 = simpleArrayListWithBasicValues();
-//        Boolean equals;
-//        //when
-//        equals = arrayList.equals(arrayList2);
-//        //then
-//        assert equals : "Test not passed in arraylist (equals)";
-//    }
-//
-//    //E get(int index);
-//    public static void givesElementOfSpecifiedPosition(List<Integer> list) {
-//        //given
-//        List<Integer> arrayList = new ArrayList<>();
-//        arrayList.add(1);
-//        Integer value;
-//        //when
-//        value = arrayList.get(0);
-//        //then
-//        assert value == 1 : "Test not passed in arraylist";
-//    }
-//
-//    //int hashCode();
-//
-//    //int indexOf(Object o);
-//    public static void returnIndexOfFirstOccurrenceOfElement(List<Integer> list) {
-//        //given
-//        List<Integer> arrayList = simpleArrayListWithBasicValues();
-//        Integer value;
-//        //when
-//        value = arrayList.indexOf(3);
-//        //then
-//        assert value == 2 : "Test not passed in arraylist";
-//    }
-//
-//    //boolean isEmpty();
-//    public static void returnsTrueIfListIsEmpty(List<Integer> list) {
-//        //given
-//        List<Integer> arrayList = new ArrayList<>();
-//        Boolean empty;
-//        //when
-//        empty = arrayList.isEmpty();
-//        //then
-//        assert empty;
-//    }
-//
-//    //boolean isEmpty();
-//    public static void returnsFalseIfListIsNotEmpty(List<Integer> list) {
-//        //given
-//        List<Integer> arrayList = simpleArrayListWithBasicValues();
-//        Boolean empty;
-//        //when
-//        empty = arrayList.isEmpty();
-//        //then
-//        assert !empty;
-//    }
 
 
 
 
 
-//    private static ArrayList<Integer> simpleArrayListWithBasicValues() {
-//        return new ArrayList<>(Arrays.asList(1,2,3));
-//    }
+
+
+
+
+    private static List<Integer> simpleListWithBasicValues(List<Integer> list) {
+        if(list instanceof ArrayList)
+            return Arrays.asList(1, 2, 3);
+        return new LinkedList<>(Arrays.asList(1, 2, 3));
+    }
+
+    private static List<Integer> emptyList(List<Integer> list) {
+        if(list instanceof ArrayList)
+            return new ArrayList<>();
+        return new LinkedList<>();
+    }
 }
 
